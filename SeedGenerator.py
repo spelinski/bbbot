6337da797e7b54ed683325da0783dc3f99760c77
Spelinski <rohk@hotmail.com>
2016-09-23 07:36:58 -0500

    order the seed formations to make it combinations instead of permutations

--- SeedGenerator.py
+++ SeedGenerator.py
@@ -2,14 +2,36 @@ import itertools
 from Formation import Formation
 import json
 
+def sort_cards(three_cards):
+    index = 1
+    while index < 3:
+        if three_cards[index][0] < three_cards[index-1][0]:
+            three_cards = list(three_cards)
+            temp_card = three_cards[index]
+            three_cards[index] = three_cards[index-1]
+            three_cards[index-1] = temp_card
+            three_cards = tuple(three_cards)
+            index = 0
+        if three_cards[index][0] == three_cards[index-1][0]:
+            if three_cards[index][1] < three_cards[index-1][1]:
+                three_cards = list(three_cards)
+                temp_card = three_cards[index]
+                three_cards[index] = three_cards[index-1]
+                three_cards[index-1] = temp_card
+                three_cards = tuple(three_cards)
+                index = 0
+        index += 1
+    return three_cards
+
 numbers = [1,2,3,4,5,6,7,8,9,10]
 suites = ["color1","color2","color3","color4","color5","color6"]
 deck = list(itertools.product(numbers,suites))
-my_side_permutations = list(itertools.permutations(deck,3))
+my_side_combinations = list(itertools.combinations(deck,3))
 other_side_combinations = list(itertools.combinations(deck,3))
 times_through_outer_loop = 0
 overall_dict = { "valid_formations": {}}
-for index_my_side,three_card_list_my_side in enumerate(my_side_permutations):
+for three_card_list_my_side in my_side_combinations:
+    three_card_list_my_side = sort_cards(three_card_list_my_side)
     this_formation_dict = { "times_won" : 0,
                             "times_lost" : 0,
                             "win_chance" : 0}
